a229785c0884e21729f6f6c8f39831e69b9ea634
muiruriiii <muiruricynthiaaa@gmail.com>
2022-06-20 22:54:14 +0300

    Insertion of policeofficer.html and criminalbooking.html data into the database complete

--- records/models.py
+++ records/models.py
@@ -17,3 +17,33 @@ class InsertUser(models.Model):
 	 address=models.CharField(max_length=100)
 	 class Meta:
 	 	db_table="tbl_citizen"
+
+
+class InsertCriminal(models.Model):
+	fName = models.CharField(max_length=100)
+	lName = models.CharField(max_length=100)
+	tel = models.CharField(max_length=100)
+	address = models.CharField(max_length=400)
+	nationalID = models.CharField(max_length=100)
+	gender = models.CharField(max_length=100)
+	crimeID = models.CharField(max_length=100)
+	criminalStatus = models.CharField(max_length=100)
+	locationArrested = models.CharField(max_length=100)
+	arrestDate = models.DateTimeField(max_length=100)
+	class Meta:
+		db_table = "tbl_criminal"
+
+class InsertOfficer(models.Model):
+	fName = models.CharField(max_length=100)
+	lName = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	tel = models.CharField(max_length=100)
+	nationalID = models.CharField(max_length=100)
+	gender = models.CharField(max_length=100)
+	DOB = models.DateTimeField(max_length=100)
+	rank = models.CharField(max_length=100)
+	password = models.CharField(max_length=400)
+	employmentStatus = models.CharField(max_length=100)
+	employmentDate = models.DateTimeField(max_length=100)
+	class Meta:
+		db_table = "tbl_police_officer"
--- records/views.py
+++ records/views.py
@@ -2,14 +2,53 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from records.models import CrimeInsert
 from records.models import InsertUser
-
+from records.models import InsertCriminal
+from records.models import InsertOfficer
 
 def policeofficer(request):
-    return render(request, 'records/policeofficer.html', {'title': 'Police Officer'})
+    if request.method == 'POST':
+        if request.POST.get('fName') and request.POST.get('lName') and request.POST.get('email') and request.POST.get('tel')  and request.POST.get('nationalID') and request.POST.get('gender') and request.POST.get(
+                'DOB')  and request.POST.get(
+                'rank') and request.POST.get('password') and request.POST.get(
+                'employmentStatus') and request.POST.get('employmentDate'):
+            saverecord = InsertOfficer()
+            saverecord.fName = request.POST.get('fName')
+            saverecord.lName = request.POST.get('lName')
+            saverecord.email = request.POST.get('email')
+            saverecord.tel = request.POST.get('tel')
+            saverecord.nationalID = request.POST.get('nationalID')
+            saverecord.gender = request.POST.get('gender')
+            saverecord.DOB = request.POST.get('DOB')
+            saverecord.rank = request.POST.get('rank')
+            saverecord.password = request.POST.get('password')
+            saverecord.employmentStatus = request.POST.get('employmentStatus')
+            saverecord.employmentDate = request.POST.get('employmentDate')
+            saverecord.save()
+            messages.success(request, 'Record Saved Successfully...!')
+            return render(request, 'records/policeofficer.html')
+    else:
+        return render(request, 'records/policeofficer.html', {'title': 'Police Officer'})
 
 
 def criminalbooking(request):
-    return render(request, 'records/criminalbooking.html', {'title': 'Criminal Booking'})
+    if request.method == 'POST':
+        if request.POST.get('fName') and request.POST.get('lName') and request.POST.get('tel') and request.POST.get('address') and request.POST.get('nationalID') and request.POST.get( 'gender') and request.POST.get('crimeID') and request.POST.get('criminalStatus') and request.POST.get('locationArrested') and request.POST.get('arrestDate'):
+            saverecord = InsertCriminal()
+            saverecord.fName = request.POST.get('fName')
+            saverecord.lName = request.POST.get('lName')
+            saverecord.tel = request.POST.get('tel')
+            saverecord.address = request.POST.get('address')
+            saverecord.nationalID = request.POST.get('nationalID')
+            saverecord.gender = request.POST.get('gender')
+            saverecord.crimeID = request.POST.get('crimeID')
+            saverecord.criminalStatus = request.POST.get('criminalStatus')
+            saverecord.locationArrested = request.POST.get('locationArrested')
+            saverecord.arrestDate = request.POST.get('arrestDate')
+            saverecord.save()
+            messages.success(request, 'Record Saved Successfully...!')
+            return render(request, 'records/criminalbooking.html')
+    else:
+        return render(request, 'records/criminalbooking.html', {'title': 'Criminal Booking'})
 
 
 def evidence(request):
